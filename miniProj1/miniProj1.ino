54293b5d037755959bb812ceae044d4e8f456126
shawnkoon <shawn9405@gmail.com>
2016-10-30 20:48:32 -0700

    Added functionality of timer that runs every .5 seconds

--- miniProj1/miniProj1.ino
+++ miniProj1/miniProj1.ino
@@ -3,42 +3,100 @@
 #define NUM_DISPLAYS 5
 #define DISPLAY_DIGITS 6
 
-//TimerOne timer;
+long displayVal[NUM_DISPLAYS] = {0L, 0L, 0L, 0L, 0L};
+String displayDigits[NUM_DISPLAYS] = {"000000", "000000", "000000", "000000", "000000"};
+bool displayRunning[NUM_DISPLAYS] = {false, false, false, false, false};
+bool allRunning = false;
 
-void displayConvert()
+TimerOne updatingTimer;
+TimerOne displayingTimer;
+
+/*
+ * This function will increment the value at given [displayNumber]by 8353
+ * Also stores corresponding score into String array of displayDigits array.
+ */
+void displayConvert(int displayNumber)
 {
-  long displayVal[NUM_DISPLAYS] = {0L, 0L, 0L, 0L, 0L};
-  char displayDigits[NUM_DISPLAYS][DISPLAY_DIGITS] = {"000000", "000000", "000000", "000000", "000000"};
-  // I thought this needed an extra char on DISPLAY_DIGITS for the terminate character...
-  // but it seems to get the values right
-  
-  for(int x = 0; x < NUM_DISPLAYS; x++)
-  {
-    while(displayVal[x]+8353 < 1000000)
-    {
-      displayVal[x] += 8353; //% 1000000 later when it continously runs?
-      Serial.println(displayVal[x]); //DEBUG
-      sprintf(displayDigits[x], "%06lu", displayVal[x]);
-      for(int test = 0; test < DISPLAY_DIGITS; test++) //DEBUG
-        Serial.print(displayDigits[x][test]); //DEBUG
-      Serial.println(); //DEBUG
-      Serial.println(); //DEBUG
-      delay(1000); //DEBUG
+    displayVal[displayNumber] += 8353;
+    
+    displayDigits[displayNumber] = String(displayVal[displayNumber]);
+}// End of displayConvert
 
-      
+
+/*
+ * This function will be ran by updatingTimer
+ */
+void updateScores()
+{
+  if(allRunning == false)
+  {
+    /*
+     * Find which display needs to start.
+     * Once found, update 3 tables accordingly.
+     * # need to search from backword. 
+     */
+     
+    int index_need_run;
+    bool index_found = false;
+    
+    for(index_need_run = (NUM_DISPLAYS - 1); index_need_run >= 0; index_need_run--)
+    {
+      if(displayRunning[index_need_run] == false)
+      {
+        index_found = true;
+        break;
+      }
     }
-    Serial.println();
+
+    if(index_found == true)
+    {
+      /*
+       * change the boolean to true and update the long display array, String display array down to that index.
+       * Ex) if found_index == 2, then update timer 5, 4, 3, 2.
+       */
+
+       displayRunning[index_need_run] = true;
+    }
+    else // if index was not found.
+    {
+      /*
+       * Set the all Running  to true.
+       * Still need to update all the numbers.
+       */
+      allRunning = true;
+      index_need_run = 0;
+    }
+
+       int index;
+       for(index = (NUM_DISPLAYS - 1); index >= index_need_run; index--)
+       {
+         displayConvert(index);
+       }
+    
   }
-}
+  else // if all of them are running.
+  {
+    /*
+     * Increment all the displayVal & change displayDigits array accordingly.
+     */
+     int index;
+     
+     for(index = NUM_DISPLAYS; index >= 0; index --)
+     {
+      displayConvert(index);
+     }
+  }
+}// End of updateScores()
 
 void setup()
 {
   Serial.begin(9600); //DEBUG
-
+  updatingTimer.initialize(500000); // timer every half second. 
+  displayingTimer.initialize(3100); // timer every 3.1 millisecond.
 }
 
 void loop()
 {
-  displayConvert();
+  updatingTimer.attachInterrupt(updateScores);
   
 }
